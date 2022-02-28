b9f1cf3d201456c3aa79df5749b6d333bc13345d
jihnkim <sysanal1697@gmail.com>
2022-02-28 16:39:42 +0900

    basic

--- practice/js/basic.js
+++ practice/js/basic.js
@@ -1,12 +1,33 @@
+// js variable
 let myPlanet;
 
 let userName;
 
-const birthday = '18.04.1982';
+const BIRTHDAY = '18.04.1982';
 
-const age = someCode(birthday);
+const age = someCode(BIRTHDAY);
 
 /*
 대문자 상수 > 하드 코딩한 값의 별칭(실행 전에 그 값을 미리 알고있는경우)
 소문자 상수 > 런타임에 평가 되는 상수(올해랑 내년나이 다르잔항)
 */
+
+// js datatype
+// no error
+let message = "hello";
+message = 123
+
+// Big Int(2^53 - 1)
+const bigInt = 212312323221321312314242n;
+
+// string
+let name = 'john';
+let name2 = "john";
+let name3 = `john`;
+
+// 역슬래시에서만 포매팅 가능
+alert(`Hello, ${name2}`);
+
+// 타입 확인
+typeof undefined
+typeof 0
