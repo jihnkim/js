9e7b8c226f95beaf80987fb0c800301aa357b37c
jihnkim <sysanal1697@gmail.com>
2022-02-28 15:40:54 +0900

    initial commit

---
+++ practice/js/basic.js
@@ -0,0 +1,12 @@
+let myPlanet;
+
+let userName;
+
+const birthday = '18.04.1982';
+
+const age = someCode(birthday);
+
+/*
+대문자 상수 > 하드 코딩한 값의 별칭(실행 전에 그 값을 미리 알고있는경우)
+소문자 상수 > 런타임에 평가 되는 상수(올해랑 내년나이 다르잔항)
+*/
